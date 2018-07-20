a59a8172d4648efaed496e8c00154661cf8132e9
Anel Drocic <anel.drocic@gmail.com>
2018-07-20 00:15:29 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+// dependecies requires
+const express     = require('express');
+const mongoose    = require('mongoose');
+const bodyParser  = require('body-parser');
+// route requires
+const itemRoute       = require('./routes/api/itemRoute');
+
+
+const app = express();
+
+
+// Connect to MongoDB using key value pair or connect to local db for production
+const db = process.env.DATABASEURI || "mongodb://localhost/mern-stack-list1";
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use('/itemRoute', itemRoute);
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
