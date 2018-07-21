c23616e292d9c641b0c6371941631c8c25370b74
Anel Drocic <anel.drocic@gmail.com>
2018-07-21 12:21:08 -0500

    cleaned up spacing in AppNavbar Component, added ShoppingList.js component and included it in App.js

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -13,13 +13,13 @@ import {
 class AppNavbar extends Component {
   state = {
     isOpen: false
-  }
+  };
 
   toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
     });
-  }
+  };
 
   render() {
     return (
@@ -27,7 +27,7 @@ class AppNavbar extends Component {
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
             <NavbarBrand href="/">Shopping List</NavbarBrand>
-            <NavbarToggler onClick={this.toggle}/>
+            <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
